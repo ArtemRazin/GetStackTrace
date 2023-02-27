f2f1e2600ff0d99a25d216cb3aa2c705ade0a3bb
Artem Razin <razin@softanics.com>
2023-02-27 18:01:09 +0300

    Initial files

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿[assembly: ArmDot.Client.ObfuscateNames()]
+
+[assembly: ArmDot.Client.VirtualizeCode()]
+
+[assembly: ArmDot.Client.ObfuscateControlFlow()]
+
+namespace GetStackTrace;
+class Program
+{
+    static void Main(string[] args)
+    {
+        function1();
+    }
+
+    static void function1()
+    {
+        function2();
+    }
+
+    static void function2()
+    {
+        throw new Exception("hi");
+    }
+}
